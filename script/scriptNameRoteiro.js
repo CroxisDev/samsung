612ce008bedc81540f221826b124dd5386920247
CroxisDev <139510943+CroxisDev@users.noreply.github.com>
2023-12-15 23:44:21 -0300

    Atualização V2

--- script/scriptNameRoteiro.js
+++ script/scriptNameRoteiro.js
@@ -1,39 +1,38 @@
 function atualizarTexto(nomeSelecionado) {
-    // Atualizar o texto para todos os parágrafos
-    for (let i = 0; i < 3; i++) {
-      const mensagemElement = document.getElementById(`mensagem${i}`);
-      const nomeSelecionadoElement = document.getElementById(`nomeSelecionado${i}`);
-      
-      nomeSelecionadoElement.textContent = nomeSelecionado || '[Seu nome]';
-    }
+  // Atualizar o texto para todos os parágrafos
+  for (let i = 0; i < 4; i++) {
+    const mensagemElement = document.getElementById(`mensagem${i}`);
+    const nomeSelecionadoElement = document.getElementById(`nomeSelecionado${i}`);
     
-    // Salvar a seleção no localStorage
-    localStorage.setItem('nomeSelecionado', nomeSelecionado);
+    nomeSelecionadoElement.textContent = nomeSelecionado || '[Seu nome]';
   }
   
-  // Carregar a seleção do nome do localStorage ao carregar a página
-  window.onload = function() {
-    const nomeSelecionado = localStorage.getItem('nomeSelecionado');
-    
-    if (nomeSelecionado) {
-      atualizarTexto(nomeSelecionado);
-    }
-  };
+  // Salvar a seleção no localStorage
+  localStorage.setItem('nomeSelecionado', nomeSelecionado);
+}
+
+// Carregar a seleção do nome do localStorage ao carregar a página
+window.onload = function() {
+  const nomeSelecionado = localStorage.getItem('nomeSelecionado');
   
-  // Esconde o dropdown no início
-  hideDropdown();
-  
-  function showDropdown() {
-    const dropdownMenu = document.getElementById('dropdownMenu');
-    dropdownMenu.style.display = 'flex';
+  if (nomeSelecionado) {
+    atualizarTexto(nomeSelecionado);
   }
-  
-  function hideDropdown() {
-    const dropdownMenu = document.getElementById('dropdownMenu');
-    dropdownMenu.style.display = 'none';
-  }
-  
-  function selecionarNome(nome) {
-    atualizarTexto(nome);
-  }
-  
+};
+
+// Esconde o dropdown no início
+hideDropdown();
+
+function showDropdown() {
+  const dropdownMenu = document.getElementById('dropdownMenu');
+  dropdownMenu.style.display = 'flex';
+}
+
+function hideDropdown() {
+  const dropdownMenu = document.getElementById('dropdownMenu');
+  dropdownMenu.style.display = 'none';
+}
+
+function selecionarNome(nome) {
+  atualizarTexto(nome);
+}
