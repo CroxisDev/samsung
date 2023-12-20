e93d724fafbcf9e6994182a4f912b89929361dae
CroxisDev <139510943+CroxisDev@users.noreply.github.com>
2023-12-20 00:01:37 -0300

    Add files via upload

---
+++ suporte v2/scriptCopySuporte.js
@@ -0,0 +1,466 @@
+// INICIO - FUNÇÃO COPIAR TEXTO
+
+function copyToClipboard(text) {
+    const input = document.createElement('textarea');
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+}
+
+// COPIAR INQUÉRITO
+
+function copyinquerito() {
+    const consumidorInfo = document.getElementById('consumidor-resposta').value;
+    const csView = document.getElementById('cs-view-resposta').value;
+    const sa = document.getElementById('sa-resposta').value;
+    const marketing = document.getElementById('marketing-resposta').value;
+    const retornoLigacao = document.getElementById('retorno-ligacao-resposta').value;
+    const protocoloSms = document.getElementById('protocolo-sms-resposta').value;
+    const visual = document.getElementById('visual-resposta').value;
+
+    let copiedText = `${consumidorInfo}\n
+        CS VIEW: ${csView}
+        SAMSUNG ACCOUNT: ${sa}
+        MARKETING: ${marketing}
+        RETORNO DE LIGAÇÃO: ${retornoLigacao}
+        PROTOCOLO POR SMS: ${protocoloSms}`;
+
+    if (visual !== '') {
+        copiedText += `\n        VISUAL SUPORTE: ${visual}`;
+    }
+        
+
+    copyToClipboard(copiedText);
+}
+
+// COPIAR RESOLUÇÃO
+
+function copyResolution() {
+const resolutionConsu = document.getElementById('informado-consumidor-resposta').value;
+
+const copiedText = `${resolutionConsu}`;
+
+copyToClipboard(copiedText);
+
+}
+
+// COPIAR LAVA E SECA
+
+function copyLavaSeca() {
+    const voltagemTomada = document.getElementById('tomada-voltagem-resposta').value;
+    const usoAdaptador = document.getElementById('adaptador-extensao-resposta').value;
+    const parafusotrans = document.getElementById('parafusos-transporte-resposta').value;
+    const pressaoagua = document.getElementById('pressao-agua-resposta').value;
+    const nivelapiso = document.getElementById('nivelamento-resposta').value;
+    const mangueiradrena = document.getElementById('mangueira-drenagem-resposta').value;
+    const modelolava = document.getElementById('etiqueta-modelo-resposta').value;
+    const filtrodetrito = document.getElementById('limpeza-filtro-detrimentos-resposta').value;
+    const filtromangueira = document.getElementById('limpeza-filtro-mangueira-resposta').value;
+    const calibralava = document.getElementById('calibracao-resposta').value;
+    const lavagemteste = document.getElementById('lavagem-teste-resposta').value;
+    const tamboreco = document.getElementById('lavagem-tambor-eco-resposta').value;
+    const resolutionConsu = document.getElementById('informado-consumidor-resposta').value;
+
+    let copiedText = `${resolutionConsu}\n
+    ➤ | LAVADORA / LAVA E SECA`;
+
+    if (voltagemTomada !== '') {
+    copiedText += `\n           • VOLTAGEM DA TOMADA: ${voltagemTomada}`;
+    }
+
+    if (usoAdaptador !== '') {
+    copiedText += `\n           • USO DE ADAPTADOR/EXTENSÃO: ${usoAdaptador}`;
+    }
+
+    if (parafusotrans !== '') {
+    copiedText += `\n           • PARAFUSOS DE TRANSPORTE: ${parafusotrans}`;
+    }
+
+    if (pressaoagua !== '') {
+    copiedText += `\n           • PRESSÃO DA ÁGUA: ${pressaoagua}`;
+    }
+
+    if (nivelapiso !== '') {
+    copiedText += `\n           • NIVELAMENTO: ${nivelapiso}`;
+    }
+
+    if (mangueiradrena !== '') {
+    copiedText += `\n           • MANGUEIRA DE DRENAGEM: ${mangueiradrena}`;
+    }
+
+    if (modelolava !== '') {
+    copiedText += `\n           • ETIQUETA CONTENDO O MODELO E/OU N° DE SÉRIE: ${modelolava}`;
+    }
+
+    if (filtrodetrito !== '') {
+    copiedText += `\n           • LIMPEZA DO FILTRO DE DETRITOS: ${filtrodetrito}`;
+    }
+
+    if (filtromangueira !== '') {
+    copiedText += `\n           • LIMPEZA DO FILTRO DA MANGUEIRA: ${filtromangueira}`;
+    }
+
+    if (calibralava !== '') {
+    copiedText += `\n           • CALIBRAÇÃO: ${calibralava}`;
+    }
+
+    if (lavagemteste !== '') {
+        copiedText += `\n           • LAVAGEM TESTE: ${lavagemteste}`;
+    }
+
+    if (tamboreco !== '') {
+    copiedText += `\n           • LAVAGEM DO TAMBOR ECO: ${tamboreco}`;
+    }
+
+    copyToClipboard(copiedText);
+
+}
+
+// COPIAR SECADORA
+
+function copySecadora() {
+    const voltagemTomada = document.getElementById('secadora-tomada-voltagem-resposta').value;
+    const usoAdaptador = document.getElementById('secadora-adaptador-extensao-resposta').value;
+    const exaustor = document.getElementById('secadora-exaustao-resposta').value;
+    const gas = document.getElementById('secadora-gas-resposta').value;
+    const nivelapiso = document.getElementById('secadora-nivelamento-resposta').value;
+    const modeloseca = document.getElementById('secadora-etiqueta-modelo-resposta').value;
+    const respiroseca = document.getElementById('secadora-respiro-resposta').value;
+    const filtroseca = document.getElementById('secadora-filtro-resposta').value;
+    const secagemteste = document.getElementById('secadora-secagem-teste-resposta').value;
+    const resolutionConsu = document.getElementById('informado-consumidor-resposta').value;
+
+    let copiedText = `${resolutionConsu}\n
+    ➤ | SECADORA`;
+
+    if (voltagemTomada !== '') {
+    copiedText += `\n           • VOLTAGEM DA TOMADA: ${voltagemTomada}`;
+    }
+
+    if (usoAdaptador !== '') {
+    copiedText += `\n           • USO DE ADAPTADOR/EXTENSÃO: ${usoAdaptador}`;
+    }
+
+    if (exaustor !== '') {
+    copiedText += `\n           • SISTEMA DE EXAUSTÃO: ${exaustor}`;
+    }
+
+    if (gas !== '') {
+    copiedText += `\n           • TUBULAÇÃO DE GÁS: ${gas}`;
+    }
+
+    if (nivelapiso !== '') {
+    copiedText += `\n           • NIVELAMENTO: ${nivelapiso}`;
+    }
+
+    if (modeloseca !== '') {
+    copiedText += `\n           • ETIQUETA CONTENDO O MODELO E/OU N° DE SÉRIE: ${modeloseca}`;
+    }
+
+    if (respiroseca !== '') {
+    copiedText += `\n           • TESTE DE OBSTRUÇÃO DO RESPIRO: ${respiroseca}`;
+    }
+
+    if (filtroseca !== '') {
+    copiedText += `\n           • LIMPEZA DO FILTRO DE FIBRAS: ${filtroseca}`;
+    }
+
+    if (secagemteste !== '') {
+    copiedText += `\n           • SECAGEM TESTE: ${secagemteste}`;
+    }
+
+    copyToClipboard(copiedText);
+
+}
+
+// COPIAR REFRIGERADOR
+
+function copyRefrigerador() {
+    const voltagemTomada = document.getElementById('refrigerador-tomada-voltagem-resposta').value;
+    const usoAdaptador = document.getElementById('refrigerador-adaptador-extensao-resposta').value;
+    const refrimodelo = document.getElementById('refrigerador-etiqueta-modelo-resposta').value;
+    const refripainel = document.getElementById('painel-led-resposta').value;
+    const refriporta = document.getElementById('borracha-porta-resposta').value;
+    const refrivedaporta = document.getElementById('vedacao-porta-resposta').value;
+    const refriquedaenergia = document.getElementById('queda-energia-resposta').value;
+    const painelcontrole = document.getElementById('config-painel-resposta').value;
+    const resolutionConsu = document.getElementById('informado-consumidor-resposta').value;
+
+    let copiedText = `${resolutionConsu}\n
+    ➤ | REFRIGERADOR`;
+
+    if (voltagemTomada !== '') {
+    copiedText += `\n           • VOLTAGEM DA TOMADA: ${voltagemTomada}`;
+    }
+
+    if (usoAdaptador !== '') {
+    copiedText += `\n           • USO DE ADAPTADOR/EXTENSÃO: ${usoAdaptador}`;
+    }
+
+    if (refrimodelo !== '') {
+    copiedText += `\n           • ETIQUETA CONTENDO O MODELO E/OU N° DE SÉRIE: ${refrimodelo}`;
+    }
+
+    if (refripainel !== '') {
+    copiedText += `\n           • PAINEL DE LED PISCANDO: ${refripainel}`;
+    }
+
+    if (refriporta !== '') {
+    copiedText += `\n           • BORRACHA DA PORTA: ${refriporta}`;
+    }
+
+    if (refrivedaporta !== '') {
+    copiedText += `\n           • VEDAÇÃO DA PORTA: ${refrivedaporta}`;
+    }
+
+    if (refriquedaenergia !== '') {
+    copiedText += `\n           • QUEDA DE ENERGIA: ${refriquedaenergia}`;
+    }
+
+    if (painelcontrole !== '') {
+        copiedText += `\n           • PAINEL DE CONTROLE: ${painelcontrole}`;
+    }
+
+    copyToClipboard(copiedText);
+
+}
+
+// COPIAR AR CONDICIONADO
+
+function copyArCondicionado() {
+const voltagemTomada = document.getElementById('arcondicionado-tomada-voltagem-resposta').value;
+const usoAdaptador = document.getElementById('arcondicionado-adaptador-extensao-resposta').value;
+const arcobre = document.getElementById('tubulacao-cobre-resposta').value;
+const armetragem = document.getElementById('metragem-tubulacao-resposta').value;
+const ardisjuntor = document.getElementById('disjuntor-exclusivo-resposta').value;
+const armodelo = document.getElementById('arcondicionado-etiqueta-modelo-resposta').value;
+const arquedaenergia = document.getElementById('arcondicionado-queda-energia-resposta').value;
+const arcontrole = document.getElementById('controle-remoto-resposta').value;
+const arfiltro = document.getElementById('filtro-resposta').value;
+const armanu = document.getElementById('manutencao-resposta').value;
+const resolutionConsu = document.getElementById('informado-consumidor-resposta').value;
+
+let copiedText = `${resolutionConsu}\n
+➤ | AR-CONDICIONADO`;
+
+if (voltagemTomada !== '') {
+copiedText += `\n           • VOLTAGEM DA TOMADA: ${voltagemTomada}`;
+}
+
+if (usoAdaptador !== '') {
+copiedText += `\n           • USO DE ADAPTADOR/EXTENSÃO: ${usoAdaptador}`;
+}
+
+if (arcobre !== '') {
+copiedText += `\n           • TUBULAÇÃO DE COBRE: ${arcobre}`;
+}
+
+if (armetragem !== '') {
+copiedText += `\n           • METRAGEM TUBULAÇÃO (3M – 15M): ${armetragem}`;
+}
+
+if (ardisjuntor !== '') {
+copiedText += `\n           • DISJUNTOR EXCLUSIVO: ${ardisjuntor}`;
+}
+
+if (armodelo !== '') {
+copiedText += `\n           • ETIQUETA CONTENDO O MODELO E/OU N° DE SÉRIE: ${armodelo}`;
+}
+
+if (arquedaenergia !== '') {
+copiedText += `\n           • QUEDA DE ENERGIA: ${arquedaenergia}`;
+}
+
+if (arcontrole !== '') {
+copiedText += `\n           • CONTROLE REMOTO: ${arcontrole}`;
+}
+
+if (arfiltro !== '') {
+copiedText += `\n           • LIMPEZA DO FILTRO: ${arfiltro}`;
+}
+
+if (armanu !== '') {
+copiedText += `\n           • MANUTENÇÃO PERIÓDICA: ${armanu}`;
+}
+
+copyToClipboard(copiedText);
+
+}
+
+// COPIAR POWERBOT
+
+function copyPowerBot() {
+const voltagemTomada = document.getElementById('powerbot-tomada-voltagem-resposta').value;
+const usoAdaptador = document.getElementById('powerbot-adaptador-extensao-resposta').value;
+const powernivel = document.getElementById('powerbot-nivelamento-resposta').value;
+const pwmodelo = document.getElementById('powerbot-etiqueta-modelo-resposta').value;
+const pwcontrole = document.getElementById('powerbotcontrole-remoto-resposta').value;
+const pwlimpeza = document.getElementById('powerbot-limpeza-resposta').value;
+const resolutionConsu = document.getElementById('informado-consumidor-resposta').value;
+
+let copiedText = `${resolutionConsu}\n
+➤ | ASPIRADOR DE PÓ`;
+
+if (voltagemTomada !== '') {
+copiedText += `\n           • VOLTAGEM DA TOMADA: ${voltagemTomada}`;
+}
+
+if (usoAdaptador !== '') {
+copiedText += `\n           • USO DE ADAPTADOR/EXTENSÃO: ${usoAdaptador}`;
+}
+
+if (powernivel !== '') {
+copiedText += `\n           • ESTAÇÃO DE LIMPEZA EM UM LOCAL NIVELADO: ${powernivel}`;
+}
+
+if (pwmodelo !== '') {
+copiedText += `\n           • ETIQUETA CONTENDO O MODELO E/OU N° DE SÉRIE: ${pwmodelo}`;
+}
+
+if (pwcontrole !== '') {
+copiedText += `\n           • CONTROLE REMOTO: ${pwcontrole}`;
+}
+
+if (pwlimpeza !== '') {
+copiedText += `\n           • LIMPEZA DE MATERIAIS ESTRANHOS: ${pwlimpeza}`;
+}
+
+copyToClipboard(copiedText);
+
+}
+
+// COPIAR LAVA-LOUÇAS
+
+function copyLavaLouca() {
+const voltagemTomada = document.getElementById('louca-tomada-voltagem-resposta').value;
+const usoAdaptador = document.getElementById('louca-adaptador-extensao-resposta').value;
+const loucanivel = document.getElementById('louca-nivelamento-resposta').value;
+const loucadreno = document.getElementById('louca-dreno-resposta').value;
+const loucamodelo = document.getElementById('louca-etiqueta-modelo-resposta').value;
+const loucasolvente = document.getElementById('louca-solventes-resposta').value;
+const loucaborracha = document.getElementById('loucaborracha-porta-resposta').value;
+const loucaveda = document.getElementById('loucavedacao-porta-resposta').value;
+const loucafiltro = document.getElementById('louca-filtro-resposta').value;
+const resolutionConsu = document.getElementById('informado-consumidor-resposta').value;
+
+let copiedText = `${resolutionConsu}\n
+➤ | LAVA-LOUÇAS`;
+
+if (voltagemTomada !== '') {
+copiedText += `\n           • VOLTAGEM DA TOMADA: ${voltagemTomada}`;
+}
+
+if (usoAdaptador !== '') {
+copiedText += `\n           • USO DE ADAPTADOR/EXTENSÃO: ${usoAdaptador}`;
+}
+
+if (loucanivel !== '') {
+copiedText += `\n           • NIVELAMENTO: ${loucanivel}`;
+}
+
+if (loucadreno !== '') {
+copiedText += `\n           • MANGUEIRA DE DRENAGEM: ${loucadreno}`;
+}
+
+if (loucamodelo !== '') {
+copiedText += `\n           • ETIQUETA CONTENDO O MODELO E/OU N° DE SÉRIE: ${loucamodelo}`;
+}
+
+if (loucasolvente !== '') {
+copiedText += `\n           • USOU SOLVENTES OU PRODUTOS DE LIMPEZA ABRASIVOS: ${loucasolvente}`;
+}
+
+if (loucaborracha !== '') {
+copiedText += `\n           • BORRACHA DA PORTA: ${loucaborracha}`;
+}
+
+if (loucaveda !== '') {
+copiedText += `\n           • VEDAÇÃO DA PORTA: ${loucaveda}`;
+}
+
+if (loucafiltro !== '') {
+copiedText += `\n           • LIMPEZA DO FILTRO: ${loucafiltro}`;
+}
+
+copyToClipboard(copiedText);
+
+}
+
+// COPIAR ORDEM DE SERVIÇO
+
+function copyOrdemServico() {
+    const modelo = document.getElementById('modelo-resposta').value;
+    const numeroSerie = document.getElementById('numero-serie-resposta').value;
+    const notaFiscal = document.getElementById('nota-fiscal-resposta').value;
+    const emGarantia = document.getElementById('garantia-resposta').value;
+    const dataCompraElement = document.getElementById('data-compra-resposta');
+    const dataCompraValue = dataCompraElement.value;
+
+    let dataCompra = '';
+
+    if (dataCompraValue) {
+    const [ano, mes, dia] = dataCompraValue.split('-');
+    dataCompra = `${dia}/${mes}/${ano}`;
+    }
+
+    let copiedText = `ABERTURA DE ORDEM DE SERVIÇO\n\n        ✍| DADOS DO PRODUTO\n`;
+
+    if (modelo !== '') {
+    copiedText += `\n           • MODELO: ${modelo}`;
+    }
+
+    if (numeroSerie !== '') {
+    copiedText += `\n           • N° DE SÉRIE: ${numeroSerie}`;
+    }
+
+    if (notaFiscal !== '') {
+    copiedText += `\n           • N° NOTA FISCAL: ${notaFiscal}`;
+    }
+
+    if (dataCompra !== '') {
+    copiedText += `\n           • DATA DA COMPRA: ${dataCompra}`;
+    }
+
+    if (emGarantia !== '') {
+    copiedText += `\n           • GARANTIA: ${emGarantia}`;
+    }
+
+    copiedText += `\n
+            CLIENTE CIENTE QUE:
+                PARA CASOS DE PRODUTOS EM PERÍODO DE GARANTIA COM POSSÍVEL EVENTO FORA DE GARANTIA PODERÁ SER GERADO UM ORÇAMENTO;
+                FORA DE GARANTIA - A VISITA, PEÇAS E MÃO DE OBRA SERÃO COBRADAS;
+                PRAZO DE 24 HORAS ÚTEIS PARA CONTATO DA ASSISTÊNCIA TÉCNICA.`;
+
+    copyToClipboard(copiedText);
+
+}
+
+// FIM - FUNÇÃO COPIAR TEXTO
+
+// INICIO - SCRIPT PARA OCULTAR LAVADORA, REFRIGERADOR, ACN, POWERBOT E LAVA-LOUÇAS
+
+    function toggleContent(id) {
+        var content = document.getElementById(id);
+        var buttons = document.getElementsByClassName('toggle-button');
+        var isContentVisible = content.style.display === 'block';
+
+        for (var i = 0; i < buttons.length; i++) {
+            var button = buttons[i];
+            var targetId = button.getAttribute('onclick').replace("toggleContent('", "").replace("')", "");
+            var targetContent = document.getElementById(targetId);
+
+            if (targetContent !== content) {
+            targetContent.style.display = 'none';
+            }
+        }
+
+        if (!isContentVisible) {
+            content.style.display = 'block';
+        } else {
+            content.style.display = 'none';
+        }
+    }
+
+// FIM - SCRIPT PARA OCULTAR LAVADORA, REFRIGERADOR, ACN, POWERBOT E LAVA-LOUÇAS
---
+++ suporte v2/scriptMainSuporte.js
@@ -0,0 +1,60 @@
+// INICIO - Exibir/Ocultar menu ao utilizar scroll do mouse
+
+const menu = document.querySelector('.menu');
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        // O usuário está rolando para cima, então mostra o menu novamente
+        menu.classList.remove('hidden');
+    } else {
+        // O usuário está rolando para baixo, então esconde o menu
+        menu.classList.add('hidden');
+    }
+
+    prevScrollPos = currentScrollPos;
+});
+
+// FIM - Exibir/Ocultar menu ao utilizar scroll do mouse
+
+// INICIO - NOTIFY
+
+// Adicionar evento de clique a todos os botões com a classe "copy-button"
+const copyButtons = document.querySelectorAll('.copy-button');
+copyButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    exibirNotificacaoDeSucesso();
+  });
+});
+
+function exibirNotificacaoDeSucesso() {
+  iziToast.success({
+    message: 'Texto copiado',
+    position: 'bottomRight',
+    timeout: 5000, // Tempo em milissegundos que a notificação será exibida (5 segundos neste exemplo)
+    closeOnClick: true, // Fechar a notificação ao clicar nela
+    pauseOnHover: true, // Pausar o tempo limite da notificação quando o mouse estiver sobre ela
+    progressBar: true, // Mostrar uma barra de progresso
+  });
+}
+
+const nameBtn = document.querySelectorAll('.notify');
+nameBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    exibirNotificacao();
+  });
+});
+
+function exibirNotificacao() {
+  iziToast.success({
+    message: 'Nome Alterado!',
+    position: 'bottomRight',
+    timeout: 5000, // Tempo em milissegundos que a notificação será exibida (5 segundos neste exemplo)
+    closeOnClick: true, // Fechar a notificação ao clicar nela
+    pauseOnHover: true, // Pausar o tempo limite da notificação quando o mouse estiver sobre ela
+    progressBar: true, // Mostrar uma barra de progresso
+  });
+}
+// FIM - Notificação
